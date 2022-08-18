66d384bc20981b013075a22204d594e7c80d4f8c
adam <tfxflrstx@gmail.com>
2022-08-18 17:32:56 -0400

    simpler syntax and simpler implementation, replaced baseclass with Enum<> to reduce polution

--- enum.h
+++ enum.h
@@ -1,46 +1,88 @@
 #pragma once
 
-#include <type_traits>
-#include <concepts>
 #include <algorithm>
 #include <string.h>
 
-template <auto N>
+//todo: replace
+template <size_t N>
 struct CompileTimeString {
     char value[N];
     consteval CompileTimeString(const char (&s)[N]) {
         std::copy_n(s, N, value);
     }
+
+    template<auto OtherN>
+    consteval bool operator ==(const CompileTimeString<OtherN>& other) const {
+        return 0 == strcmp(value, other.value);
+    }
 };
 
-struct _EnumBase {};
+/**
+ * @brief specialization for <>, only used as grandfather class
+ */
+template<CompileTimeString... MoreValues>
+class Enum {
+    protected:
+        consteval Enum(){}
+
+        template<size_t SetCount>
+        static consteval size_t indexOf(char const * const (&set)[SetCount], const char * const toFind) {
+            size_t index = 0;
+            for (const auto& s : set)
+                if (0 == strcmp(s, toFind))
+                    return index; else ++index;
+            throw "value not present in set";
+        };
+};
 
 template<CompileTimeString FirstValue, CompileTimeString... MoreValues>
-struct Enum : _EnumBase {
-    static constexpr char const * const values[] = {FirstValue.value, MoreValues.value...};
+class Enum<FirstValue, MoreValues...> : private Enum<> {
+    protected:
+        const size_t i;
+
+        consteval Enum(size_t i) : i(i) {}
+
+        static consteval size_t indexOf(const char * const toFind) {
+            return Enum<>::indexOf(values, toFind);
+        };
+
+    public:
+        static constexpr const char * const values[] = {FirstValue.value, MoreValues.value...};
+        
+        consteval Enum(const char * const s) : i(indexOf(s)) {}
+
+        constexpr bool operator==(const Enum& other) const { return i == other.i; }
+        constexpr bool operator==(const Enum&& other) const { return i == other.i; }
+
+        constexpr operator char const*const() const { return values[i]; }
+        constexpr operator size_t() const { return i; }
 };
 
-template<std::derived_from<_EnumBase> Base, CompileTimeString FirstValue, CompileTimeString... MoreValues>
-struct Subset : Base {
+template<class Base, CompileTimeString FirstValue, CompileTimeString... MoreValues>
+requires (std::is_base_of_v<Enum<>,Base>)
+class Subset : public Base {
     private:
         template <size_t supersetN, size_t subsetN>
-        static constexpr bool isSubset(char const * const (&superset)[supersetN], char const * const (&subset)[subsetN]) {
-            for (const auto& subItem : subset) {
-                bool found = false;
-                for (const auto& superItem : superset) {
-                    if (0 == strcmp(subItem, superItem)) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
+        static consteval bool isSubset(const char * const (&superset)[supersetN], const char * const(&subset)[subsetN]) {
+            for (const auto& s : subset)
+                if (666 == Enum<>::indexOf(superset, s))//todo: replace 666
                     return false;
-                }
-            }
             return true;
         }
-        static_assert((isSubset(Base::values, {FirstValue.value, MoreValues.value...})), "subset values not all present in the base set");
+
+        static_assert(
+            (isSubset(Base::values, {FirstValue.value, MoreValues.value...})), 
+            "subset values not all present in the base set"
+        );
+
+    protected:
+        consteval Subset(size_t i) : Base(i) {}
 
     public:
         static constexpr char const * const values[] = {FirstValue.value, MoreValues.value...};
+        
+        consteval Subset(const char * const s) : Subset(Base::indexOf(s)) {
+            if (Base::i == 666)//todo: replace 666
+                throw "value not present in subset";
+        }
 };
--- main.cpp
+++ main.cpp
@@ -2,22 +2,38 @@
 #include <iostream>
 
 int main() {
-    typedef Enum<"red", "blue", "green", "yellow"> Color;
-    typedef Subset<Color, "red", "blue"> LColor;
+    typedef Enum<"red", "blue", "green", "yellow", "purple"> Color;
+    typedef Enum<"black", "white", "gray", "darkGray", "lightGray", "offWhite"> Shade;
+    typedef Subset<Color, "red", "green"> LColor;
+    typedef Subset<Color, "green", "yellow", "purple"> L2Color;
+    typedef Subset<L2Color, "green", "yellow"> L3Color;
+    
+    auto a = Color("green");
+    Color aa = "green";
+    auto b = LColor("green");
+    auto c = L2Color("green");
+    auto d = L3Color("gre5en");
 
-    for (auto& c : Color::values)
-        std::cout << c << std::endl;
+    switch (Color("purple")){
+        case Shade("offWhite"):
+        case Color("red"):
+            std::cout << "r" << std::endl;
+            break;
+        case Color("blue"):
+            std::cout << "b" << std::endl;
+            break;
+        case Color("green"):
+            std::cout << "g" << std::endl;
+            break;
+        case Color("yellow"):
+            std::cout << "y" << std::endl;
+            break;
+        case Color("purple"):
+            std::cout << "p" << std::endl;
+            break;
+    }
 
-    std::cout << std::endl;
-
-    for (auto& c : LColor::values)
-        std::cout << c << std::endl;
-
-    std::cout << std::endl;
-
-    for (auto& c : Subset<Color, "yellow">::values)
-        std::cout << c << std::endl;
-
+    std::cout << (b == d) << std::endl;
 
     return 0;
 }
